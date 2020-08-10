4791ff6c3241462f445bd024de4edb4b9df6dbe4
ARC19-dev <65944186+ARC19-dev@users.noreply.github.com>
2020-08-10 23:05:35 +0430

    Add files via upload

---
+++ App/test.py
@@ -0,0 +1,29 @@
+from config import *
+
+test_logger = logging.getLogger('Test.Core')
+tlogger = test_logger
+
+# tlogger.debug('This is a debug msg') # 10
+# tlogger.info('This is a info msg') # 20
+# tlogger.warning('This is a warning msg') # 30
+# tlogger.error('This is a error msg') # 40
+# tlogger.critical('This is a critical msg') # 50
+
+logger.debug('This is a debug msg') # 10
+logger.info('This is a info msg') # 20
+logger.warning('This is a warning msg') # 30
+logger.error('This is a error msg') # 40
+logger.critical('This is a critical msg') # 50
+                                                            
+
+print('logged successfully')
+
+loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
+print(loggers)
+
+
+# todo: solve the problem of the logging (logger logs two times)
+# todo: start the search engine (or the search core)
+
+
+
